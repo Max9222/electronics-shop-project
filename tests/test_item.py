7d2b14a7a5efa7819109a5a75138aa8e34638490
Max9222 <maxoog@bk.ru>
2023-07-12 21:56:14 +0300

    add item.py add test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def items():
+    return Item("Смартфон", 10000, 20)
+
+def test_item_sum(items):
+    """ Проверяем общую стоимотсть товара стоимость товара"""
+    assert items.calculate_total_price() == 200000
+
+def test_item_discount(items):
+    """ Проверяем при скидке (скидки нет) конкретного товара"""
+    assert items.apply_discount() == None
